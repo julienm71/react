0da8155ecb492faca635a004c68d33cdaafad736
Julien Martin <julienm.contact@gmail.com>
2019-11-14 16:01:24 +0100

    design

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,10 +5,10 @@ class Banner extends React.Component {
     return (
       <div className="banner">
         <div className="container">
-          <h1>Bienvenue à <strong>PrimateLand</strong> !</h1>
-          <p>Ici vous trouverez tout les grands primates qui ont existé.</p>
+          <h1 className="some-title3">Bienvenue à <strong>PrimateLand</strong> !</h1>
+          <p className="some-p">Ici vous trouverez tout les grands primates qui ont existé.</p>
           <a href="#more" className="btn btn-primary">En savoir plus</a>
-          <a href="#primates" className="btn btn-success ml-2">Tout les primates</a>
+          <a href="#primates" className="btn btn-success ml-2">Tous les primates</a>
         </div>
         <p className="scrolldown">
            <a title="En savoir plus" href="#more"><i className="fas fa-chevron-circle-down"></i></a>
